b951fa87e816716afb1fb0df6082ba8ef53b5ee9
carlosm4247 <carlosm4247@gmail.com>
2025-03-16 18:46:57 -0400

    adding svr file

---
+++ svr.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVR
+
+#def svr_train_predict(x_train, y_train, x_test):
+    
