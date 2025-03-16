109b58e4d3581686b3e83eedf45c4da0a3aae02a
carlosm4247 <carlosm4247@gmail.com>
2025-03-16 19:00:48 -0400

    added code for svr model

--- svr.py
+++ svr.py
@@ -3,5 +3,37 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVR
 
-#def svr_train_predict(x_train, y_train, x_test):
-    
+def svr_train_predict(x_train, y_train, x_test):
+    # Scale the data
+    scaler_X = StandardScaler()
+    scaler_Y = StandardScaler()
+
+    X_train_scaled = scaler_X.fit_transform(x_train)
+    X_test_scaled = scaler_X.transform(x_test)
+
+    Y_train_scaled = scaler_Y.fit_transform(y_train.values.reshape(-1, 1))
+
+    #Create the Model
+    model = SVR()
+
+    # Define the hyperparameters to tune
+    param_grid = {
+        'kernel': ['linear', 'rbf', 'sigmoid'],
+        'C': [1, 10],
+        'gamma': ['scale']
+    }
+
+    # GridSearchCV to get the best model
+    gridSearch = GridSearchCV(estimator= model, param_grid= param_grid, cv= 3, n_jobs= -1, verbose= 2)
+
+    # Fit the model
+    gridSearch.fit(X_train_scaled, Y_train_scaled.ravel())
+
+    model = gridSearch.best_estimator_  # return the BEST model
+
+    # Predictions
+    predictions_scaled = model.predict(X_test_scaled)
+    # Inverse the scaling to Orignal Values
+    predictions = scaler_Y.inverse_transform(predictions_scaled.reshape(-1, 1))
+
+    return predictions
