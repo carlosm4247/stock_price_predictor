8e9e8d9b6f91733e4a7d7c76b2c08cf596e767b7
carlosm4247 <carlosm4247@gmail.com>
2025-03-16 19:06:36 -0400

    added randomForest file

---
+++ randomForest.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+
+#def randomForest_train_predict(x_train, y_train, x_test):
+
